d77ed27a13299b2806f145c7fd44d05160f4c4c2
klay66 <klaytop112233@gmail.com>
2024-09-15 17:15:43 +0300

    first commit

---
+++ src/components/Progress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Progress({ index, numQuestions, maxPossiblePoints, points, answer }) {
+    return (
+        <header className='progress'>
+            <progress max={numQuestions} value={index + Number(answer !== null)} />
+            <p>Questions <strong>{index + 1}</strong>/{numQuestions}</p>
+
+            <p><strong>{points}</strong>/{maxPossiblePoints}</p>
+        </header>
+    )
+}
